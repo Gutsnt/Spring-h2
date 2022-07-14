b1b075c7d8cd9b01ab2fc6b5e5caa06dc663a3e5
Charlie Mendoza <charlie.mendoza1337@gmail.com>
2022-07-13 20:54:00 -0400

    update

---
+++ src/main/java/com/h2/h2api/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.h2.h2api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+//lombok
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String nombre;
+	@Column(nullable = false)
+	private String apellido;
+	@Column(length = 11)
+	private String phone;
+	private String direction;
+}
